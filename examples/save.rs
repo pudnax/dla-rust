7e4b95a780603f3a97490712996cf52b7893eb3a
komm <k.a.komissar@gmail.com>
2019-08-17 14:16:11 +0600

    clean

--- examples/save.rs
+++ examples/save.rs
@@ -1,15 +1,15 @@
 use dla::Builder;
 
 fn main() {
-    let mut model = Builder::flat();
+    let mut model = Builder::convex();
 
     model.add([60., 0.].into(), 0);
     model.add([-60., 0.].into(), 1);
 
-    for _ in 0..10000 {
+    for _ in 0..1000 {
         model.add_particle();
     }
 
     model.save_csv("out.csv").unwrap();
-    model.render("render3.png");
+    model.render("render4.png");
 }
